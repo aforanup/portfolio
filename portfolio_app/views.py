d65713815ae22a45e11e8e64abf4be32127e3df3
aforanup <aforanupn@gmail.com>
2022-01-24 17:33:53 +0545

    template directory changes

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 def index(request):
     context = {}
-    return render(request, 'home.html', context)
+    return render(request, 'portfolio_app/home.html', context)
