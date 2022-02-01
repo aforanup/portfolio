71e138397356cf13bfd6be2be7751c34bfdc18b1
aforanup <aforanupn@gmail.com>
2022-02-01 23:23:22 +0545

    index page changes

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 def index(request):
     context = {}
     return render(request, 'portfolio_app/home.html', context)
+
+def home(request):
+    context = {}
+    return render(request, 'portfolio_app/home.html', context)
